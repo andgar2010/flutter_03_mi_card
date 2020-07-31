9041e505703dfa04247037d2cbe44389bcf4efe5
Andres Garcia - Mac <dev@tech-andgar.me>
2020-07-31 00:44:08 -0500

    refactor and style

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.lightBlue[900],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -25,45 +26,64 @@ class MyApp extends StatelessWidget {
               Text(
                 'Andrés García',
                 style: TextStyle(
-                    fontFamily: 'Play', color: Colors.white, fontSize: 40),
+                  fontFamily: 'Play',
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(3.0, 3.0),
+                      blurRadius: 3.0,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    Shadow(
+                      offset: Offset(5.0, 5.0),
+                      blurRadius: 8.0,
+                      color: Color.fromARGB(125, 0, 0, 255),
+                    ),
+                  ],
+                ),
               ),
               Text(
                 'Mobile App Flutter and FullStack Developer',
                 style: TextStyle(
-                    fontFamily: 'Noto Sans TC',
-                    color: Colors.grey.shade400,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5),
+                  fontFamily: 'Noto Sans TC',
+                  color: Colors.lightBlue.shade100,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                color: Colors.white,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.lightBlue[900],
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '+57 317 3323 476 ONLY TEXT',
-                        style: TextStyle(
-                          color: Colors.lightBlue[900],
-                          fontFamily: 'Noto Sans PT',
-                          fontSize: 15,
-                        ),
-                      )
-                    ],
-                  ),
+              SizedBox(
+                height: 15,
+                width: 275,
+                child: Divider(
+                  color: Colors.lightBlue[500],
                 ),
               ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlue[900],
+                  ),
+                  title: Text(
+                    '+57 317 3323 476 ONLY TEXT',
+                    style: TextStyle(
+                      color: Colors.lightBlue[900],
+                      fontFamily: 'Noto Sans PT',
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
                 color: Colors.white,
                 child: ListTile(
                   leading: Icon(
